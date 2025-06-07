7c73b6794f4d89bec4c626fc26fc8845660a363c
AbdugofurKomilobv <komilovabdugofur635@gmail.com>
2025-06-07 18:09:10 +0500

    ff

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
-son1 = input('son1: ')
-son2 = input('son2: ')
-if not son2.isdigit():
-    print('faqt raqam kiriting')
-operator = input('+,*,/,-,: ')
+# son1 = input('son1: ')
+# son2 = input('son2: ')
+# if not son2.isdigit():
+#     print('faqt raqam kiriting')
+# operator = input('+,*,/,-,: ')
 
 
 
 
-if operator == '+':
-    print(son1+son2)
-elif operator == '/':
-    if son2 != "0":
-        print(son1 / son2)
-    else:
-        print(son1,"ni ",son2, 'ga bolib bolmaydi')
+# if operator == '+':
+#     print(son1+son2)
+# elif operator == '/':
+#     if son2 != "0":
+#         print(son1 / son2)
+#     else:
+#         print(son1,"ni ",son2, 'ga bolib bolmaydi')
    
-elif operator == '*':
-    print(son1*son2)
-elif operator == '-':
-    print(son1-son2)
-else:
-    print('soz notogri belgi kiritdingiz')
+# elif operator == '*':
+#     print(son1*son2)
+# elif operator == '-':
+#     print(son1-son2)
+# else:
+#     print('soz notogri belgi kiritdingiz')
+
+# ===================================
+
+
+# Ternar operatorlar
+# bu bir qatorli kod sharlar if else da ishlaydi 
+# age = 1
+
+# print('siz voyaga yetgansiz' if age >= 18 else 'siz voyaga yetmagansiz')
